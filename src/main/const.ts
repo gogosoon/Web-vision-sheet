e3dc97e0bcfafef2417e9f457540fe5a9df5fa29
Dineshkumar A <dineshkumar.a@gogosoon.com>
2025-05-01 23:33:41 +0530

    [Saran] updated the api url to const

--- src/main/const.ts
+++ src/main/const.ts
@@ -1,6 +1,8 @@
-import dotenv from 'dotenv'
-dotenv.config()
-const web_app_url = process.env.VITE_WEB_APP_URL;
+// import dotenv from 'dotenv'
+// dotenv.config()
+// in production build .env is not loaded properly
+// const web_app_url = process.env.VITE_WEB_APP_URL;
+const web_app_url = "https://spreadsheetflow.com";
 console.log('web_app_url', web_app_url);
 export const CONST_ELECTON_APP = {
     APP_PROTOCOL: 'spreadsheetflow',
