cfd3dfaa8c8afa3ec6a407595a397af30c823ac4
Dineshkumar A <dineshkumar.a@gogosoon.com>
2025-05-12 17:49:54 +0530

    [DJ] FIx errors on close and reopen

--- src/main/index.ts
+++ src/main/index.ts
@@ -104,7 +104,52 @@ function createWindow(): void {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+}
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  // Set app user model id for windows
+  electronApp.setAppUserModelId('com.electron')
+
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  // Create the singleton WebService instance
+  webServiceInstance = new WebService()
+
+  // Set up IPC handlers
+  setupIpcHandlers()
+
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  // Ensure Puppeteer browser is closed on quit
+  webServiceInstance
+    ?.closeBrowser()
+    .catch((err) => console.error('Failed to close browser on app quit:', err))
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// IPC handlers setup
+function setupIpcHandlers() {
   // IPC Handlers for authentication
   ipcMain.handle('auth:check-protocol-registration', () => {
     return app.isDefaultProtocolClient(CONST_ELECTON_APP.APP_PROTOCOL)
@@ -188,52 +233,6 @@ function createWindow(): void {
       }
     }
   })
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  // Create the singleton WebService instance
-  webServiceInstance = new WebService()
-
-  // Set up IPC handlers
-  setupIpcHandlers()
-
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  // Ensure Puppeteer browser is closed on quit
-  webServiceInstance
-    ?.closeBrowser()
-    .catch((err) => console.error('Failed to close browser on app quit:', err))
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-// IPC handlers setup
-function setupIpcHandlers() {
   // Open file dialog
   ipcMain.handle('dialog:openFile', async () => {
     const { canceled, filePaths } = await dialog.showOpenDialog({
